ec5c169904f9df14189860369aa093bc182eb8ff
David Durbin <David-Durbin@users.noreply.github.com>
2019-02-01 11:29:49 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // variables
+        byte yTest = 7;
+        short sTest = 14;
+        int iTest = 21;
+        long lTest = 28;
+        float fTest = 35;
+        double dTest = 42;
+        boolean bTest = false;
+        char cTest = 'S';
+
+        // display variables
+        System.out.println("byte test    = " + bTest);
+        System.out.println("short test   = " + sTest);
+        System.out.println("int test     = " + iTest);
+        System.out.println("long test    = " + lTest);
+        System.out.println("float test   = " + fTest);
+        System.out.println("double test  = " + dTest);
+        System.out.println("boolean test = " + bTest);
+        System.out.println("char test    = " + cTest);
+    }
+}
